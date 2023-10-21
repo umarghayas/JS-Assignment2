fe6ac90f77f9708b73fc32faf5ec3b69631a07b8
Umar Ghayas <umar.ghayas2000@gmail.com>
2023-10-21 16:35:04 +0500

    CHAPTER 17 - 20 DONE

--- app.js
+++ app.js
@@ -154,4 +154,134 @@
 // } //DONE
 
 
-// Chapter 13 - 15
+// Chapter 13 - 15 DONE
+
+// Chapter 17 - 20 DONE
+// 1. Declare and initialize an empty multidimensional array.
+// (Array of arrays)
+
+// var arr = [
+//     ["Hasan", 21],
+//     ["Jaffar", 23],
+//     ["Hunain", 25]
+// ] //DONE
+
+//2. Declare and initialize a multidimensional array 
+// representing the following matrix:
+
+// var matrix = [
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1]
+// ]
+
+// console.log(matrix) //DONE
+
+// 3. Write a program to print numeric counting from 1 to 10.
+// for(var i=1;i<=10;i++){
+//     document.write(i+"<br />")
+// }  //DONE
+
+// 4. Write a program to print multiplication table of any 
+// number using for loop. Table number & length should be 
+// taken as an input from user
+// var num = +prompt("Enter a number to show its multiplication table")
+// var len = +prompt("Enter length of multiplication table")
+// for (var i = 1; i <= len; i++) {
+//     document.write(num + " x " + i + " = " + num * i + "<br />")
+// }  //DONE
+
+// Write a program to print items of the following array 
+// using for loop:
+// fruits = ["apple", "banana", "mango", "orange", "strawberry"]
+// document.write(fruits + "<br />")
+// for(var i=0;i<=fruits.length;i++){
+//     document.write("Element at index "+i+" is "+fruits[i]+"<br />")
+// }  // DONE 
+
+// Generate the following series in your browser. See 
+// example output.
+// a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+// document.write("<h5>Counting: </h5>")
+// for(var i=1;i<=15;i++){
+//     document.write(i + ",")
+// }
+// document.write("<br />")
+// // b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+// document.write("<h5>Reverse Counting</h5>")
+// for(var i=10;i>=1;i--){
+//     document.write(i + ",")
+// }
+// document.write("<br />")
+
+// // c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+// document.write("<h5>Even: </h5>")
+// for(var i=0;i<=20;i+=2){
+//     document.write(i + ",")
+// }
+// document.write("<br />")
+
+// // d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
+// document.write("<h5>Odd: </h5>")
+// for(var i=1;i<=20;i+=2){
+//     document.write(i + ",")
+// }
+// document.write("<br />")
+// // e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
+// document.write("<h5>Even: </h5>")
+// for(var i=2;i<=20;i+=2){
+//     document.write(i + "k, ")
+// }   //DONE 
+
+// 7. You have an array
+// A = ["cake", "apple pie", "cookie", "chips", "patties"]
+// Write a program to enable "search by user input" in an 
+// array.
+// After searching, prompt the user whether the given item is
+// found in the list or not. Example
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"]
+
+// var flag = false
+// var ind;
+// var userSearch = prompt("Welcome to ABC Bakery. What would you like to order?")
+// for (var i = 0; i <= A.length; i++) {
+//     if (A[i] === userSearch) {
+//         flag = true
+//         ind = i
+//         break;
+//     }
+// }
+
+// if (flag == false) {
+//     document.write("We are Sorry. " + userSearch + "is not available in our bakery")
+// }
+// else {
+//     document.write(userSearch + "is available at index " + ind + " in our bakery")
+// }  // DONE 
+
+
+// Write a program to identify the largest number in the 
+// given array.
+// A = [24, 53, 78, 91, 12]
+// document.write("Array Items: " + A + "<br>")
+
+// A.sort()
+
+// document.write("Largest Number is : " + A[A.length - 1])   //DONE
+
+
+// 9. Write a program to identify the smallest number in the 
+// given array.
+// A = [24, 53, 78, 91, 12]
+// document.write("Array Items: " + A + "<br>")
+// A.sort()
+// document.write("Smallest Number is : " + A[0])   //DONE
+
+
+// 10. Write a program to print multiples of 5 ranging 1 to 100+
+// for(var i=5;i<=100;i+=5){
+//     document.write(i + " , ")
+// }  //DONE
+
+// CHAPTER 17 - 20 DONE
+
